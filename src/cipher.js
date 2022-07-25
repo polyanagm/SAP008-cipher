9abf8ba165e08e902f2b2cade2d919932ba8be85
Polyanagm <polyana.gm@hotmail.com>
2022-07-25 17:33:31 -0300

    Primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
-const cipher = {
-  // ...
-};
+const cipher = {encode,decode}
+
+function encode(offset, mensagem) {
+  let newText = ""
+  for (let i = 0; i < mensagem.length; i++) {
+    let codifica = ((mensagem.charCodeAt(i) - 65 + offset) %26) + 65;
+    let textoPronto = String.fromCharCode(codifica);
+    newText += textoPronto;
+  }  
+  return newText;
+}
+
+function decode(offset,mensagem){
+ let newText = ""
+ for (let i = 0; i < mensagem.length; i++) {
+   let decodifica = ((mensagem.charCodeAt(i) - 65 - offset) %26) + 65;
+   let textoPronto = String.fromCharCode(decodifica);
+   newText += textoPronto
+ }
+ return newText;
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let senha = document.getElementById("senha");
+let btnCodificar = document.getElementById("codifica");
+let btnDecodifica = document.getElementById("decodifica");
+let deslocamento = document.getElementById("deslocamento");
+let senhapronta = document.getElementById("senhapronta");
+
+function codifica(){
+     let senha = document.getElementById("senha").value
+     senha = senha.toUpperCase()
+     let result = ""
+     let deslocamento = parseInt(document.getElementById("deslocamento").value);
+     let resultado = cipher.encode(deslocamento, senha);
+     document.getElementById("senhapronta").innerHTML = "Sua senha criptografada irá aparecer aqui: " + '"' + resultado + '"'
+}
+ 
+document.getElementById("codifica").addEventListener("click", codifica);
+
+function decode(){
+     let senha = document.getElementById("senha").value
+     senha = senha.toUpperCase()
+     let result = ""
+     let deslocamento = parseInt(document.getElementById("deslocamento").value);
+     let resultado = cipher.decode(deslocamento,senha);
+     document.getElementById("senhapronta").innerHTML = "Sua senha criptografada irá aparecer aqui: " + '"' + resultado + '"'
+}
+
+document.getElementById("decodifica").addEventListener("click", decode);
