0db27fe6df140359294c7332023d87cff5426c56
Polyanagm <polyana.gm@hotmail.com>
2022-07-27 17:54:23 -0300

    testes ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,37 @@
 const cipher = {encode,decode}
 
 function encode(offset, mensagem) {
+  if (typeof offset != "number"){
+    throw TypeError("Necessário escolher um número")
+  }
+
+  if (typeof mensagem != "string"){
+    throw TypeError("Necessário digitar uma mensagem")
+  }
   let newText = ""
   for (let i = 0; i < mensagem.length; i++) {
-    let codifica = ((mensagem.charCodeAt(i) - 65 + offset) %26) + 65;
+    let codifica = ((mensagem[i].charCodeAt(0) - 65 + offset) %26) + 65;
     let textoPronto = String.fromCharCode(codifica);
     newText += textoPronto;
   }  
   return newText;
 }
 
-function decode(offset,mensagem){
- let newText = ""
- for (let i = 0; i < mensagem.length; i++) {
-   let decodifica = ((mensagem.charCodeAt(i) - 65 - offset) %26) + 65;
+function decode(offset,mensagem) {
+  if (typeof offset != "number"){
+    throw TypeError()
+  }
+
+  if (typeof mensagem != "string"){
+    throw TypeError()
+  }
+  let newText = ""
+  for (let i = 0; i < mensagem.length; i++) {
+   let decodifica = (mensagem.charCodeAt(i) - 90 - offset) %26 + 90;
    let textoPronto = String.fromCharCode(decodifica);
-   newText += textoPronto
+   newText += textoPronto;
  }
  return newText;
-
 }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import cipher from './cipher.js';
+const textInput = document.getElementById("senha");
 
-let senha = document.getElementById("senha");
-let btnCodificar = document.getElementById("codifica");
-let btnDecodifica = document.getElementById("decodifica");
-let deslocamento = document.getElementById("deslocamento");
-let senhapronta = document.getElementById("senhapronta");
+textInput.addEventListener("keypress",function(e) {
+
+    if(!checkChar(e)){
+        e.preventDefault()
+    }
+});
+function checkChar(e) {
+    const char = String.fromCharCode(e.keyCode);
+    const pattern = "[A-Z]";
+
+    if(char.match(pattern)){
+
+    return true;
+    }
+}
 
 function codifica(){
      let senha = document.getElementById("senha").value
      senha = senha.toUpperCase()
-     let result = ""
      let deslocamento = parseInt(document.getElementById("deslocamento").value);
      let resultado = cipher.encode(deslocamento, senha);
-     document.getElementById("senhapronta").innerHTML = "Sua senha criptografada irá aparecer aqui: " + '"' + resultado + '"'
+     document.getElementById("senhapronta").innerHTML = "Sua senha codificada/decodificada irá aparecer aqui: " + '"' + resultado + '"'
 }
  
 document.getElementById("codifica").addEventListener("click", codifica);
@@ -20,10 +30,9 @@ document.getElementById("codifica").addEventListener("click", codifica);
 function decode(){
      let senha = document.getElementById("senha").value
      senha = senha.toUpperCase()
-     let result = ""
      let deslocamento = parseInt(document.getElementById("deslocamento").value);
      let resultado = cipher.decode(deslocamento,senha);
-     document.getElementById("senhapronta").innerHTML = "Sua senha criptografada irá aparecer aqui: " + '"' + resultado + '"'
+     document.getElementById("senhapronta").innerHTML = "Sua senha codificada/decodificada irá aparecer aqui: " + '"' + resultado + '"'
 }
 
 document.getElementById("decodifica").addEventListener("click", decode);
