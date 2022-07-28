c468a2add2c443acd81500ff720e657af770360f
Polyanagm <polyana.gm@hotmail.com>
2022-07-27 23:13:50 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,38 @@
+import cipher from './cipher.js';
+const textInput = document.getElementById("senha");
+
+textInput.addEventListener("keypress",function(e) {
+
+    if(!checkChar(e)){
+        e.preventDefault()
+    }
+});
+function checkChar(e) {
+    const char = String.fromCharCode(e.keyCode);
+    const pattern = "[A-Z]";
+
+    if(char.match(pattern)){
+
+    return true;
+    }
+}
+
+function codifica(){
+     let senha = document.getElementById("senha").value
+     senha = senha.toUpperCase()
+     let deslocamento = parseInt(document.getElementById("deslocamento").value);
+     let resultado = cipher.encode(deslocamento, senha);
+     document.getElementById("senhapronta").innerHTML = "Sua senha codificada/decodificada irá aparecer aqui: " + '"' + resultado + '"'
+}
+ 
+document.getElementById("codifica").addEventListener("click", codifica);
+
+function decode(){
+     let senha = document.getElementById("senha").value
+     senha = senha.toUpperCase()
+     let deslocamento = parseInt(document.getElementById("deslocamento").value);
+     let resultado = cipher.decode(deslocamento,senha);
+     document.getElementById("senhapronta").innerHTML = "Sua senha codificada/decodificada irá aparecer aqui: " + '"' + resultado + '"'
+}
+
+document.getElementById("decodifica").addEventListener("click", decode);
